60ff2a4d741b37f457c1fa5b7f0aa9e36b2da0ce
Anthony Aylward <aaylward@eng.ucsd.edu>
2018-09-05 17:09:39 -0700

    test

--- R/beta_bern_sum.R
+++ R/beta_bern_sum.R
@@ -23,7 +23,7 @@ region <- function(x, size) {
 
 #' @title Density of a sum with \code{prob}/\code{rho} parametrization
 #'
-#' @param x number of successes
+#' @param x vector of quantiles
 #' @param size vector giving the number of trials for each group.
 #' @param prob vector giving probability of success for each group.
 #' @param rho vector giving correlation parameter for each group.
@@ -34,33 +34,38 @@ density_of_sum_prob_rho <- function(x, size, prob = 0.5, rho = 0, ...) {
   if (!all(sapply(list(size, prob, rho), length) == 2)) {
     stop("bad argument lengths")
   }
-  sum(
-    apply(
-      region(x, size),
-      1,
-      function(row) {
-        prod(
-          sapply(
-            c(1, 2),
-            function(index) {
-              dbetabinom(
-                row[[index]],
-                size[[index]],
-                prob = prob[[index]],
-                rho = rho[[index]],
-                ...
+  sapply(
+    x,
+    function(x_i) {
+      sum(
+        apply(
+          region(x_i, size),
+          1,
+          function(row) {
+            prod(
+              sapply(
+                c(1, 2),
+                function(index) {
+                  dbetabinom(
+                    row[[index]],
+                    size[[index]],
+                    prob = prob[[index]],
+                    rho = rho[[index]],
+                    ...
+                  )
+                }
               )
-            }
-          )
+            )
+          }
         )
-      }
-    )
+      )
+    }
   )
 }
 
 #' @title Density of a sum with \code{shape1}/\code{shape2} parametrization
 #'
-#' @param x number of successes
+#' @param x vector of quantiles
 #' @param size vector giving the number of trials for each group.
 #' @param shape1,shape2 the two (positive) shape parameters of the standard
 #'   beta distribution. See the documentation for \code{Betabinom} in the
@@ -72,33 +77,38 @@ density_of_sum_shape1_shape2 <- function(x, size, shape1, shape2, ...) {
   if (!all(sapply(list(size, shape1, shape2), length) == 2)) {
     stop("bad argument lengths")
   }
-  sum(
-    apply(
-      region(x, size),
-      1,
-      function(row) {
-        prod(
-          sapply(
-            c(1,2),
-            function(index) {
-              dbetabinom.ab(
-                row[[index]],
-                size[[index]],
-                shape1 = shape1[[index]],
-                shape2 = shape2[[index]],
-                ...
+  sapply(
+    x,
+    function(x_i) {
+      sum(
+        apply(
+          region(x_i, size),
+          1,
+          function(row) {
+            prod(
+              sapply(
+                c(1,2),
+                function(index) {
+                  dbetabinom.ab(
+                    row[[index]],
+                    size[[index]],
+                    shape1 = shape1[[index]],
+                    shape2 = shape2[[index]],
+                    ...
+                  )
+                }
               )
-            }
-          )
+            )
+          }
         )
-      }
-    )
+      )
+    }
   )
 }
 
 #' @title Density of a sum with \code{prob}/\code{shape1} parametrization
 #'
-#' @param x number of successes
+#' @param x vector of quantiles
 #' @param size vector giving the number of trials for each group.
 #' @param prob vector giving probability of success for each group.
 #' @param shape1 vector giving correlation parameter for each group.
@@ -109,27 +119,32 @@ density_of_sum_prob_shape1 <- function(x, size, prob, shape1, ...) {
   if (!all(sapply(list(size, prob, shape1), length) == 2)) {
     stop("bad argument lengths")
   }
-  sum(
-    apply(
-      region(x, size),
-      1,
-      function(row) {
-        prod(
-          sapply(
-            c(1,2),
-            function(index) {
-              dbetabinom.ab(
-                row[[index]],
-                size[[index]],
-                shape1 = shape1[[index]],
-                shape2 = shape1[[index]] * (1 - prob[[index]]) / prob[[index]],
-                ...
+  sapply(
+    x,
+    function(x_i) {
+      sum(
+        apply(
+          region(x, size),
+          1,
+          function(row) {
+            prod(
+              sapply(
+                c(1,2),
+                function(index) {
+                  dbetabinom.ab(
+                    row[[index]],
+                    size[[index]],
+                    shape1 = shape1[[index]],
+                    shape2 = shape1[[index]]*(1-prob[[index]])/prob[[index]],
+                    ...
+                  )
+                }
               )
-            }
-          )
+            )
+          }
         )
-      }
-    )
+      )
+    }
   )
 }
 
