f0776fbeb92b413dad6a2e3b5a0fa35034ae77d6
Anthony Aylward <aaylward@eng.ucsd.edu>
2018-11-07 16:14:10 -0800

    export region

--- R/region.R
+++ R/region.R
@@ -8,6 +8,7 @@
 #' @param size vector giving the number of trials for each group.
 #' @return a matrix with two columns, the rows of which are the coordinates of
 #'   the region of integration.
+#' @export
 region <- function(x, size) {
   if (x < 0 || x > sum(size)) stop("provided x is out of bounds")
   coord1 <- min(0, x - size[[2]]):min(x, size[[1]])
