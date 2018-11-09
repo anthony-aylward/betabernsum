3d1f23b1247c1170a0bff3a3573b3a998a99d9c4
Anthony Aylward <aaylward@eng.ucsd.edu>
2018-11-08 17:43:12 -0800

    update region function to handle sums of 3 or more samples

--- R/region.R
+++ R/region.R
@@ -2,15 +2,49 @@
 # region.R
 #===============================================================================
 
+# Imports ======================================================================
+
+#' @import partitions
+
+
+
+
+# Functions ====================================================================
+
 #' @title Region of integration for bernoulli sums
 #'
 #' @param x number of successes
 #' @param size vector giving the number of trials for each group.
-#' @return a matrix with two columns, the rows of which are the coordinates of
-#'   the region of integration.
-#' @export
+#' @return a matrix with one column per sample, the rows of which are the
+#'   coordinates of the region of integration.
 region <- function(x, size) {
   if (x < 0 || x > sum(size)) stop("provided x is out of bounds")
-  coord1 <- max(0, x - size[[2]]):min(x, size[[1]])
-  cbind(coord1, x - coord1)
+  size_sorted <- sort(size, decreasing = TRUE)
+  parts <- diffparts(x)[1:length(size),, drop = FALSE]
+  parts <- parts[,
+    apply(
+      parts,
+      2,
+      function(col) {
+        all(col <= size_sorted) && (sum(col) >= x)
+      }
+    )
+  ]
+  parts <- do.call(
+    cbind,
+    lapply(
+      apply(parts, 2, function(col) {unique(permn(col))}),
+      function(l) {do.call(cbind, l)}
+    )
+  )
+  parts <- parts[,
+    apply(
+      parts,
+      2,
+      function(col) {
+        all(col <= size)
+      }
+    )
+  ]
+  t(parts)
 }
