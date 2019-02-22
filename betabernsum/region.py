b85f6e9c3b1897c980284ccb5fe1e2b838cae46e
anthony-aylward <aaylward@eng.ucsd.edu>
2019-02-22 01:10:26 -0800

    update

--- betabernsum/region.py
+++ betabernsum/region.py
@@ -12,37 +12,36 @@ from accelasc import accel_asc
 
 # Functions ====================================================================
 
-def region(x, size):
+def region(k, n):
     """Region of integration for bernoulli sums
 
     Parameters
     ----------
-    x : int
+    k : int
         number of successes
-    size
+    n
         iterable of integers, giving the number of trials for each group
     
-    Returns
+    Yields
     -------
-    numpy array
-        an array with one column per sample, the rows of which are the
-        coordinates of the region of integration.
+    tuple
+        a set of coordinates in the region
     """
 
-    if x < 0 or x > sum(size):
-        raise RuntimeError('provided x is out of bounds')
-    if x == 0:
-        yield (0,) * len(size)
+    if k < 0 or k > sum(n):
+        raise RuntimeError('provided k is out of bounds')
+    if k == 0:
+        yield (0,) * len(n)
         return
-    if x == sum(size):
-        yield tuple(size)
+    if k == sum(n):
+        yield tuple(n)
         return
     yield from itertools.chain.from_iterable(
         set(
             permutation for permutation in itertools.permutations(
-                partition + [0] * (len(size) - len(partition))
+                partition + [0] * (len(n) - len(partition))
             )
-            if all((p <= s for p, s in zip(permutation, size)))
+            if all((p <= s for p, s in zip(permutation, n)))
         )
-        for partition in accel_asc(x) if len(partition) <= len(size)
+        for partition in accel_asc(k) if len(partition) <= len(n)
     )
