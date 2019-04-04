f9457b6c591313c708248c036e89c940c9f3efea
anthony-aylward <aaylward@eng.ucsd.edu>
2019-04-03 21:40:54 -0700

    test

--- betabernsum/bbs.py
+++ betabernsum/bbs.py
@@ -82,11 +82,9 @@ def bbs_cdf(
         return 1.0
 
     if k <= sum(n) / 2:
-        speed_flip = False
-        r = range(k + 1)
+        speed_flip, r = False, range(k + 1)
     else:
-        speed_flip = True
-        r = range(sum(n) - k + 1)
+        speed_flip, r, a, b = True, range(sum(n) - k), b, a
 
     if len(r) > max_iter:
         if graceful:
